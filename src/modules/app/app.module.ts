fe4ed7644fe9568d92b29067446b9dacfe244db7
Vladyslav Pavlov <vladpavlov9957@gmail.com>
2021-07-17 01:05:06 +0300

    updated imports for app module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule.forRoot()],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+        useCreateIndex: true,
+        useNewUrlParser: true,
+      }),
+      inject: [ConfigService],
+    }),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule.forRoot()],
+      useFactory: async (configService: ConfigService) => ({
+        ttl: configService.get<number>('THROTTLE_TTL'),
+        limit: configService.get<number>('THROTTLE_LIMIT'),
+      }),
+      inject: [ConfigService],
+    }),
+    ScheduleModule.forRoot(),
+  ],
 })
 export class AppModule {}
